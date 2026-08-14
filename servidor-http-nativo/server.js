488087ce3c5b59dc7cef7b69eb7a3cdc08d846cd
Jean Lindo <jean.lucas.asantana@gmail.com>
2026-08-14 21:27:52 +0000

    feat: Solving the questions from slide 1

--- servidor-http-nativo/server.js
+++ servidor-http-nativo/server.js
@@ -1,22 +1,41 @@
 import http from 'node:http'
+import { URL } from 'node:url'
 
 const porta = 3000
 
-const server = http.createServer();
+const server = http.createServer()
 
+const requisicao = (req, res) => {
+    console.log(new Date().toISOString());
 
-    const requisicao = (req, res) => {
-   console.log('Requisição recebida! ${req.method} ${req.url}');
+    const urlObj = new URL(req.url, `http://${req.headers.host}`);
 
-res.statusCode = 201
-res.setHeader('Content-Type', 'text/plain charse= utf-8');
-//'application/json'
-res.end("Recurso Criado");
-//JSON.stringify({"Mensagem":"Valor"})
+    if (req.method === 'GET' && urlObj.pathname === '/saudacao') {
+        const nome = urlObj.searchParams.get('nome');
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        return res.end(JSON.stringify({ nome }));
+    }
+
+    if (req.method === 'GET' && urlObj.pathname === '/produtos') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        return res.end(JSON.stringify({ data: 'Pipito' }));
+    }
+
+    if (req.method === 'GET' && urlObj.pathname === '/contatos') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        return res.end(JSON.stringify({ data: [{ telefone: '67 3441-8922' }, { 'e-mail': 'pirilampo@gmail.com' }] }));
+    }
+
+    res.statusCode = 201;
+    res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+    return res.end('Recurso criado');
 }
 
 server.on('request', requisicao);
 
 server.listen(porta, () => {
-    console.log('Servidor ouvindo na porta ${porta}')
+    console.log(`Servidor ouvindo na porta ${porta}`)
 });
